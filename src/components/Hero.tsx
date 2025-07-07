d687cd07f42b1335d4dce07e82adf4b81be59bdc
Matue556 <mvktrm7@gmail.com>
2025-07-07 20:12:49 -0300

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -55,7 +55,7 @@ const Hero = () => {
           <div className="w-full mx-auto px-3 sm:px-6 lg:px-8 flex flex-col items-center justify-center min-h-[60vh] sm:min-h-full pt-10">
             <motion.div className="w-full max-w-4xl text-center" variants={itemVariants}>
               {/* Título principal sem margens superiores */}
-              <motion.h1 className="text-white text-lg leading-tight sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-bold mb-2 sm:mb-8 px-1 sm:px-2" variants={itemVariants} style={{ textShadow: '2px 2px 4px rgba(0, 0, 0, 0.9)' }}>
+              <motion.h1 className="text-white text-2xl leading-tight sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-bold mb-2 sm:mb-8 px-1 sm:px-2" variants={itemVariants} style={{ textShadow: '2px 2px 4px rgba(0, 0, 0, 0.9)' }}>
                 Destrave seu Potencial com o<br className="block" /> 
                 Guia Definitivo Contra a<br className="block" /> 
                 Procrastinação
