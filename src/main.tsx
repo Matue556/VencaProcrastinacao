c33f2ae515f4471660c69adc0c5933b87470ab5b
Matue556 <mvktrm7@gmail.com>
2025-07-09 17:39:48 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -9,3 +9,27 @@ createRoot(document.getElementById("root")!).render(
     <App />
   </HelmetProvider>
 );
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js')
+    .then(function(reg) {
+      console.log('Service Worker registrado com sucesso!', reg.scope);
+    })
+    .catch(function(err) {
+      console.log('Erro ao registrar o Service Worker:', err);
+    });
+}
+const brevoScript = document.createElement('script');
+brevoScript.src = 'https://cdn.brevo.com/js/sdk-loader.js';
+brevoScript.async = true;
+document.head.appendChild(brevoScript);
+
+brevoScript.onload = () => {
+  // Inicializa quando o script carregar
+  (window as any).Brevo?.push?.([
+    "init",
+    {
+      client_key: new URLSearchParams(window.location.search).get("kee") || "",
+    },
+  ]);
+};
