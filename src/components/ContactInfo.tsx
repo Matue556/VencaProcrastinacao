3fc38d78c60055888901e77e3be09d63dbef2477
Matue556 <mvktrm7@gmail.com>
2025-08-09 22:19:34 -0300

    Update ContactInfo.tsx

--- src/components/ContactInfo.tsx
+++ src/components/ContactInfo.tsx
@@ -123,7 +123,7 @@ const OfferCard = () => {
                 <p className="text-xl sm:text-xl md:text-lg text-gray-200 leading-relaxed">
                   <strong>Você só precisa dar<br />o primeiro passo.</strong>
                 </p>
-                <p className="font-bold text-white text-lg sm:text-xl md:text-lg">
+                <p className="font-bold text-white text-xl sm:text-xl md:text-lg">
                   Adquira seu acesso<br/>
                   E quebre as correntes de uma vez por todas.
                 </p>
